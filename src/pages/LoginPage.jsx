c65f1e07c4ab339e675162b43b189466765cd448
Cristian <97061228+cristians-12@users.noreply.github.com>
2023-10-13 15:21:49 -0500

    Add files via upload

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -25,13 +25,13 @@ const LoginPage = () => {
       })}
       <form
         onSubmit={onSubmit}
-        className="flex flex-col mx-auto w-2/6 gap-3 justify-center mt-[5%]"
+        className="flex flex-col mx-auto md:w-2/6 gap-3 justify-center mt-[5%] sm:w-[80%]"
       >
         <input
           type="email"
           name="email"
           // id=""
-          className="rounded p-1 border border-gray-300 focus:border-orange-500 focus:ring-blue-200 outline-none"
+          className="rounded p-1 border border-gray-300 focus:border-orange-500 focus:ring-blue-200 outline-none "
           {...register("email", { required: true })}
           placeholder="Email"
         />
