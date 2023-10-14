8dbcb3f9b5b2eada2e91b3c339697d0d76123b7d
Cristian <97061228+cristians-12@users.noreply.github.com>
2023-10-13 22:23:17 -0500

    Add files via upload

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -67,13 +67,19 @@ const LoginPage = () => {
           ¡Unete!
         </Link>
       </p>
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" className="absolute bottom-0">
-        <path
-          fill="#ff5500"
-          // fill-opacity="1"
-          d="M0,96L80,133.3C160,171,320,245,480,234.7C640,224,800,128,960,112C1120,96,1280,160,1360,192L1440,224L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"
-        ></path>
-      </svg>
+      <div className="overflow-hidden w-screen">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 1440 320"
+          className="absolute bottom-0"
+        >
+          <path
+            fill="#ff5500"
+            // fill-opacity="1"
+            d="M0,96L80,133.3C160,171,320,245,480,234.7C640,224,800,128,960,112C1120,96,1280,160,1360,192L1440,224L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"
+          ></path>
+        </svg>
+      </div>
     </main>
   );
 };
